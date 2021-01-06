42c6d7abaf9ab2cca606894d9590a9169bf3392d
Kanmii <maneptha@gmail.com>
2021-01-06 04:36:28 +0100

    Seems fzf.vim puts in .bashrc - so do not do in .fzf.sh
    Will confirm later if this is actually true when doing fresh
    provisioning of ubuntu

--- .fzf.sh
+++ .fzf.sh
@@ -4,7 +4,7 @@
 RG_OPTIONS="--hidden --follow --glob '!{.git,cover,coverage,.elixir_ls,deps,_build,.build,build}'"
 
 # fzf fuzzy finder
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+# [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 FZF_PREVIEW_APP="--preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -300'"
 export FZF_DEFAULT_OPTS="--layout=reverse --border --bind alt-j:down,alt-k:up $FZF_PREVIEW_APP"
 # Use git-ls-files inside git repo, otherwise rg
