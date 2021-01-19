d37e655afc91a1fa30ddb332a74396cfac5e923c
Kanmii <maneptha@gmail.com>
2021-01-19 03:12:19 +0100

    .bash_append.sh: add function to launch codi vim plugin

--- .bash_append.sh
+++ .bash_append.sh
@@ -24,3 +24,17 @@ case $SHELL in
 esac
 
 [ -f $HOME/dotfiles/.fzf.sh ] && source $HOME/dotfiles/.fzf.sh
+
+# Codi
+# Usage: codi [filetype] [filename]
+codi() {
+  local syntax="${1:-python}"
+  shift
+  vim -c \
+    "let g:startify_disable_at_vimenter = 1 |\
+    set bt=nofile ls=0 noru nonu nornu |\
+    hi ColorColumn ctermbg=NONE |\
+    hi VertSplit ctermbg=NONE |\
+    hi NonText ctermfg=0 |\
+    Codi $syntax" "$@"
+}
