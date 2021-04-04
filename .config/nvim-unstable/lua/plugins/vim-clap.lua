bff45287ef83ebaad5c13bbaefac3fcc3d8aad4e
Kanmii <maneptha@gmail.com>
2021-04-04 03:06:39 -0400

    nvim 0.5 wip

---
+++ .config/nvim-unstable/lua/plugins/vim-clap.lua
@@ -0,0 +1,44 @@
+" Search file from root directory
+nnoremap <Leader>ff :Clap files ++finder=rg --files --follow --hidden --glob $RG_IGNORES<CR>
+" Search file from current directory
+nnoremap <silent> <Leader>. :Clap files <C-r>=expand("%:h")<CR>/<CR>
+" find open buffers
+nnoremap <Leader>fb :Clap buffers<CR>
+" Files managed by git
+nnoremap <Leader>fg :Clap git_files<CR>
+" search buffers history
+nnoremap <Leader>hh :Clap command_history<CR>
+" search for text in current buffer
+nnoremap <Leader>bl :Clap blines<CR>
+" search for text in loaded buffers
+nnoremap <Leader>L :Clap lines<CR>
+nnoremap <Leader>mm :Clap marks<CR>
+" commands: user defined, plugin defined, or native commands
+nnoremap <Leader>C :Clap command<CR>
+" key mappings - find already mapped before defining new mappings
+nnoremap <Leader>M :Clap maps<CR>
+nnoremap <leader>ft :Clap filetypes<CR>
+" search in project
+nnoremap <Leader>/ :Clap grep2<CR>
+" Git commits
+nnoremap <leader>cm :Clap commits<CR>
+" Git commits for the current buffer
+nnoremap <leader>bc :Clap bcommits<CR>
+nnoremap <leader>fw :Clap windows<CR>
+nnoremap <leader>fs :Clap colors<CR>
+" find tags in current buffer: requires liuchengxu/vista.vim
+nnoremap <leader>bt :Clap tags<CR>
+" find tags in entire project directory
+nnoremap <leader>pt :Clap proj_tags<CR>
+" Ivy-like file explorer:
+" use tab to enter directory
+" bksp go up directory
+nnoremap <Leader>fe :Clap filer<CR>
+nnoremap <leader>y :Clap yanks<CR>
+nnoremap <leader>fq :Clap quickfix<CR>
+
+let g:clap_theme = 'material_design_dark'
+
+let g:clap_layout = {
+  \ 'relative': 'editor'
+  \ }
---
+++ .config/nvim-unstable/lua/plugins/vim-vsnip.lua
@@ -0,0 +1,4 @@
+let g:vsnip_filetypes = {}
+let g:vsnip_snippet_dir = '~/.config/nvim/lsp-snippets'
+let g:vsnip_filetypes.javascriptreact = ['javascript']
+let g:vsnip_filetypes.typescriptreact = ['typescript']
