d0104445d87d6cc015d297a46e1b46d3a56f2627
Kanmii <maneptha@gmail.com>
2021-04-06 14:07:22 +0200

    wip2

--- .config/nvim-unstable/lua/plugins/init.lua
+++ .config/nvim-unstable/lua/plugins/init.lua
@@ -1,9 +1,7 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
--- load packer as optional plugin i.e. plugins will be loaded on demand
--- https://stackoverflow.com/questions/48700563/how-do-i-install-plugins-in-neovim-correctly
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
   execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
@@ -11,72 +9,125 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 return require('packer').startup(function()
-  -- Packer can manage itself as an optional plugin
-  use {'wbthomason/packer.nvim', opt = true}
+  -- Packer can manage itself
+  use {
+    'wbthomason/packer.nvim',
+  }
 
-  -- Modify * to also work with visual selections.
-  use 'nelstrom/vim-visual-star-search'
-  -- Toggle comments in various ways.
-  use 'tpope/vim-commentary'
-  -- A number of useful motions for the quickfix list, pasting and more.
-  use 'tpope/vim-unimpaired'
-  use 'tpope/vim-surround'
-  -- Connect to database use vim
-  use 'tpope/vim-dadbod'
-  use 'ntpeters/vim-better-whitespace'
-  -- Surround text with quotes, parenthesis, brackets, and more.
-  use 'easymotion/vim-easymotion'
-  use 'diepm/vim-rest-console'
-  -- interactive scratchpad  = repl
-  use 'metakirby5/codi.vim'
-  -- Align Markdown table
-  use 'godlygeek/tabular'
-  use {'iamcco/markdown-preview.nvim',  run = 'cd app & yarn install'}
-  -- let g:mkdp_refresh_slow = 1
+  -- GENERAL
+  use {
+    -- Modify * to also work with visual selections.
+    'nelstrom/vim-visual-star-search',
+    -- Toggle comments in various ways.
+    'b3nj5m1n/kommentary',
+    -- A number of useful motions for the quickfix list, pasting and more.
+    'tpope/vim-unimpaired',
+    'tpope/vim-surround',
+    -- Connect to database use vim
+    'tpope/vim-dadbod',
+    'ntpeters/vim-better-whitespace',
+    -- Surround text with quotes, parenthesis, brackets, and more.
+    'easymotion/vim-easymotion',
+    'diepm/vim-rest-console',
+    -- interactive scratchpad  = repl
+    'metakirby5/codi.vim',
+    -- Align Markdown table
+    'godlygeek/tabular',
 
-  -- MANAGE VIM SESSIONS AUTOMACTICALLY
-  use 'tpope/vim-obsession'
-  use 'dhruvasagar/vim-prosession'
+    {
+      'iamcco/markdown-preview.nvim',
+      run = 'cd app & yarn install'
+    },
+    -- let g:mkdp_refresh_slow = 1
+
+    -- MANAGE VIM SESSIONS AUTOMACTICALLY
+    'tpope/vim-obsession',
+    'dhruvasagar/vim-prosession',
+  }
 
   -- GIT
-  -- A git wrapper so awesome it should be illegal.
-  use 'tpope/vim-fugitive'
-  use 'airblade/vim-gitgutter'
-  use 'will133/vim-dirdiff'
+  use {
+    "lewis6991/gitsigns.nvim"
+  }
 
   -- FUZZY FINDER
-  use {'liuchengxu/vim-clap',  run = ':Clap install-binary!'}
+    -- Telescope
+  use {
+    "nvim-telescope/telescope.nvim",
+    requires = {
+      -- An implementation of the Popup API from vim in Neovim
+      "nvim-lua/popup.nvim" ,
+      -- for lua development in NEOVIM - the plugin is not useful outside neovim
+      -- This plugin defines re usable functions like those found in
+      -- python's batteries included. popup.nvim depends on it.
+      "nvim-lua/plenary.nvim" ,
+      "nvim-telescope/telescope-fzy-native.nvim" ,
+      "nvim-telescope/telescope-media-files.nvim"
+    }
+  }
+
   -- View and search LSP symbols, tags in Vim/NeoVim.
   use 'liuchengxu/vista.vim'
 
   -- THEMES
-  use 'rakr/vim-one'
-  use 'lifepillar/vim-gruvbox8'
-  use 'lifepillar/vim-solarized8'
+  use {
+    "sainnhe/sonokai",
+    "wadackel/vim-dogrun",
+    "christianchiarulli/nvcode-color-schemes.vim",
+    "Th3Whit3Wolf/one-nvim",
+    "Th3Whit3Wolf/space-nvim",
+    "sainnhe/edge",
+    {
+      "npxbr/gruvbox.nvim",
+      requires = "rktjmp/lush.nvim"
+    }
+  }
 
   -- SYNTAX HIGHLIGHTING
-  -- We recommend updating the parsers on update
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+  use {
+    {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate'
+    },
+    "windwp/nvim-ts-autotag",
+    "p00f/nvim-ts-rainbow"
+  }
 
   -- VIM STATUS BAR / TABS / WINDOWS
-  -- cool status bar
-  use 'itchyny/lightline.vim'
-  -- Floaterm is a floating terminal for Neovim
-  use 'voldikss/clap-floaterm' -- use clap to search floaterm
-  use 'voldikss/vim-floaterm'
+  use {
+    -- tabline plugin with re-orderable, auto-sizing, clickable tabs, etc
+    "romgrk/barbar.nvim",
+    -- statusline plugin. you can use the api provided by galaxyline to create
+    -- the statusline that you want, easily
+    "glepnir/galaxyline.nvim",
+    -- Floaterm is a floating terminal for Neovim
+    "voldikss/vim-floaterm"
+  }
 
   -- SNIPPET ENGINES
-  use 'hrsh7th/vim-vsnip'
-  use 'hrsh7th/vim-vsnip-integ'
-  -- COMPLETION
-  use 'hrsh7th/nvim-compe'
+  use {
+    'hrsh7th/vim-vsnip',
+    'hrsh7th/vim-vsnip-integ'
+  }
 
   -- LANGUAGE SERVERS / SYNTAX CHECKING
-  use 'neovim/nvim-lspconfig'
+  use {
+    'neovim/nvim-lspconfig',
+    -- COMPLETION
+    'hrsh7th/nvim-compe'
+  }
 
-  -- for lua development in NEOVIM - the plugin is not useful outside neovim
-  -- This plugin defines re usable functions like those found in
-  -- python's batteries included
-  use 'nvim-lua/plenary.nvim'
+  -- CODE FORMATTERS
+  use {
+    -- Neoformat uses a variety of formatters for many filetypes
+    "sbdchd/neoformat",
+  }
 
+  -- File manager
+  use {
+      "kyazdani42/nvim-tree.lua",
+      requires = {
+        "kyazdani42/nvim-web-devicons"
+      }
+  }
 end)
