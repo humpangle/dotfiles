3b52a2c126b3b4cefe65be7af33e6643917df3d0
Kanmii <maneptha@gmail.com>
2021-04-10 23:23:05 +0200

    .wsl.sh: not need to append windows system32 to path
    Looks like that has already been taken care of by microsoft

--- .wsl.sh
+++ .wsl.sh
@@ -4,7 +4,7 @@ if [ -n "$WSL_DISTRO_NAME" ]; then
   # following needed so that cypress browser testing can work in WSL2
   export DISPLAY="$(/sbin/ip route | awk '/default/ { print $3 }'):0"
   # without the next line, linux executables randomly fail in TMUX in WSL
-  export PATH="$PATH:/c/WINDOWS/system32"
+  # export PATH="$PATH:/c/WINDOWS/system32"
 
   alias wslexe='/c/WINDOWS/system32/wsl.exe '
   alias ubuntu20='/c/WINDOWS/system32/wsl.exe --distribution Ubuntu-20.04'
