756f635f9f09e7908b858e6f7c97b230f6f37d0e
Kanmii <maneptha@gmail.com>
2021-04-12 09:50:52 +0200

    move nvim-compe settings out of completion.lua

--- .config/nvim-unstable/lua/lsp/completion.lua
+++ .config/nvim-unstable/lua/lsp/completion.lua
@@ -22,33 +22,6 @@ Vim.fn.sign_define("LspDiagnosticsSignHint", {
     numhl = "LspDiagnosticsSignHint",
 })
 
-require"compe".setup {
-    enabled = Completion.enabled,
-    autocomplete = true,
-    debug = false,
-    min_length = 1,
-    preselect = "enable",
-    throttle_time = 80,
-    source_timeout = 200,
-    incomplete_delay = 400,
-    max_abbr_width = 100,
-    max_kind_width = 100,
-    max_menu_width = 100,
-    documentation = true,
-    source = {
-        path = Completion.path,
-        buffer = Completion.buffer,
-        calc = Completion.calc,
-        vsnip = Completion.snippets,
-        nvim_lsp = Completion.lsp,
-        nvim_lua = {kind = "  "},
-        spell = Completion.spell,
-        tags = true,
-        -- treesitter = true,
-        emoji = {kind = " ﲃ "},
-    },
-}
-
 -- symbols for autocomplete
 Vim.lsp.protocol.CompletionItemKind = {
     "   ",
