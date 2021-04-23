0472577e5843f44c52ed5fc44eddb1fae9647ac6
Kanmii <maneptha@gmail.com>
2021-04-23 04:11:07 +0200

    vim lua: update floaterm plugin settings

--- .config/nvim-unstable/lua/plugins/vim-floaterm.lua
+++ .config/nvim-unstable/lua/plugins/vim-floaterm.lua
@@ -4,13 +4,15 @@ Vimg.floaterm_keymap_toggle = "<F1>"
 Vimg.floaterm_keymap_next = "<F2>"
 Vimg.floaterm_keymap_prev = "<F3>"
 Vimg.floaterm_keymap_new = "<F4>"
-Vimg.floaterm_gitcommit = "floaterm"
 Vimg.floaterm_autoinsert = 1
 Vimg.floaterm_width = 0.5
-Vimg.floaterm_height = 0.9
+Vimg.floaterm_height = 0.999999
 Vimg.floaterm_wintitle = 0
 Vimg.floaterm_autoclose = 1
 Vimg.floaterm_position = "topright"
 
+-- float | split | vsplit
+-- Vimg.floaterm_wintype='vsplit'
+
 u.map("n", "<leader>Fk", ":FloatermKill<CR>")
-u.map("n", "<leader>FF", ":FloatermNew")
+u.map("n", "<leader>FF", ":FloatermNew ")
