d9936f8ea3549407858f9a7c4009fab2fe373d82
Kanmii <maneptha@gmail.com>
2021-04-25 07:48:07 +0200

    vim lsp:tweak keymaps to suit my workflow

--- .config/nvim-unstable/lua/lsp.lua
+++ .config/nvim-unstable/lua/lsp.lua
@@ -44,39 +44,54 @@ local function on_attach(client, bufnr)
     -- Mappings.
     local opts = {noremap = true, silent = true}
     buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+
     buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+
     buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+
     buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>",
-                   opts)
+
+    -- buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>",
+    --                opts)
+
     buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>",
                    opts)
+
     buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>",
                    opts)
+
     buf_set_keymap("n", "<leader>law",
                    "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+
     buf_set_keymap("n", "<leader>lrw",
                    "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+
     buf_set_keymap("n", "<leader>llw",
                    "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
                    opts)
+
     buf_set_keymap("n", "<leader>lt",
                    "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+
     buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+
     buf_set_keymap("n", "<leader>lrf", "<cmd>lua vim.lsp.buf.references()<CR>",
                    opts)
+
     buf_set_keymap("n", "<leader>ld",
                    "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>",
                    opts)
-    buf_set_keymap("n", "<leader>ll",
+
+    buf_set_keymap("n", "<leader>dd",
                    "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-    buf_set_keymap("n", "<leader>lca", "<cmd>lua vim.lsp.buf.code_action()<CR>",
-                   opts)
+
+    buf_set_keymap("n", ",ac", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 
     -- Set some keybinds conditional on server capabilities
     if client.resolved_capabilities.document_formatting then
         buf_set_keymap("n", "<leader>lf",
                        "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
     elseif client.resolved_capabilities.document_range_formatting then
         buf_set_keymap("n", "<leader>lf",
                        "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
@@ -102,13 +117,16 @@ local nvim_lsp = require("lspconfig")
 local lsp_configs = require("lspconfig/configs")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
+-- https://github.com/hrsh7th/nvim-compe#how-to-use-lsp-snippet
 capabilities.textDocument.completion.completionItem.snippetSupport = true;
+capabilities.textDocument.completion.completionItem.resolveSupport =
+    {properties = {"documentation", "detail", "additionalTextEdits"}}
 
 -- Emmet
 -- https://github.com/aca/emmet-ls
+-- npm i -g vscode-html-languageserver-bin
 -- npm i -g emmet-ls
 -- no need for hmtl server having emmet-ls and snippets working
--- npm i -g vscode-html-languageserver-bin
 -- nvim_lsp.html.setup {}
 lsp_configs.emmet_ls = {
     default_config = {
