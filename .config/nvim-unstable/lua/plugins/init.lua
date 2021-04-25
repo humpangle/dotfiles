4462286e35cacc22a3926fc9591712c3cd8d6de1
Kanmii <maneptha@gmail.com>
2021-04-25 07:39:56 +0200

    vim lua:packer installation:use global variables - trivial

--- .config/nvim-unstable/lua/plugins/init.lua
+++ .config/nvim-unstable/lua/plugins/init.lua
@@ -1,13 +1,9 @@
----@diagnostic disable: undefined-global
-local execute = vim.api.nvim_command
-local fn = vim.fn
+local install_path = Vimf.stdpath("data") ..
+                         "/site/pack/packer/start/packer.nvim"
 
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-
-if fn.empty(fn.glob(install_path)) > 0 then
-    execute("!git clone https://github.com/wbthomason/packer.nvim " ..
-                install_path)
-    execute "packadd packer.nvim"
+if Vimf.empty(Vimf.glob(install_path)) > 0 then
+    Cmd("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+    Cmd "packadd packer.nvim"
 end
 
 return require("packer").startup(function()
