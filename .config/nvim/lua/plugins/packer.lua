ab5a9494fc029635444474170a0e0e56dcc9cc8b
Kanmii <maneptha@gmail.com>
2021-05-03 00:09:56 +0200

    vim:add easymotion plugin

--- .config/nvim/lua/plugins/packer.lua
+++ .config/nvim/lua/plugins/packer.lua
@@ -5,7 +5,8 @@ local install_path = vim.fn.stdpath("data") ..
                          "/site/pack/packer/start/packer.nvim"
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    vim.cmd("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+    vim.cmd("!git clone https://github.com/wbthomason/packer.nvim " ..
+                install_path)
     vim.cmd("packadd packer.nvim")
 end
 
@@ -92,4 +93,5 @@ return require("packer").startup(function()
     use {"sbdchd/neoformat"}
 
     use {"folke/which-key.nvim"}
+    use {"easymotion/vim-easymotion"}
 end)
