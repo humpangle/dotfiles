5d0fcacd976f670512618a1df16282da34194dbc
Kanmii <maneptha@gmail.com>
2021-08-02 07:59:58 +0200

    shell alias for reboot

--- .regular_shell.sh
+++ .regular_shell.sh
@@ -152,6 +152,8 @@ alias eshell='exec $SHELL'
 alias exshell='export SHELL=/usr/bin/bash'
 alias rmvimswap='rm ~/.local/share/nvim/swap/*'
 alias hb='sudo systemctl hibernate'
+# debian package `lrzsz`
+alias rb='sudo reboot'
 alias luamake=/home/kanmii/.local/bin/lua/sumneko/lua-language-server/3rd/luamake/luamake
 
 export DOCKER_BUILDKIT=1
