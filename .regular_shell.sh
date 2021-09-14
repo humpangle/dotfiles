2bc090a206dca18d4a22bc7ea560606981215f3e
Kanmii <maneptha@gmail.com>
2021-09-14 08:06:50 +0200

    shell alias to clear memory in wsl - clear shell also

--- .regular_shell.sh
+++ .regular_shell.sh
@@ -262,7 +262,7 @@ if [ -n "$WSL_DISTRO_NAME" ]; then
   # This is specific to WSL 2. If the WSL 2 VM goes rogue and decides not to free
   # up memory, this command will free your memory after about 20-30 seconds.
   #   Details: https://github.com/microsoft/WSL/issues/4166#issuecomment-628493643
-  alias dpc="sudo sh -c \"echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'\""
+  alias dpc="clear && sudo sh -c \"echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'\""
 fi
 
 # The minimal, blazing-fast, and infinitely customizable prompt for any shell!
