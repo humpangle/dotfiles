3702ca48c2f9d2f71bc39d8c85614f30bd657de7
Kanmii <maneptha@gmail.com>
2022-04-13 22:46:12 +0200

    shell alias to install nvim packer

--- regular_shell.sh
+++ regular_shell.sh
@@ -124,6 +124,10 @@ alias vff.='export EBNIS_VIM_FUZZY_FINDER='
 alias vfff='export EBNIS_VIM_FUZZY_FINDER=fzf'
 alias vffc='export EBNIS_VIM_FUZZY_FINDER=vim-clap'
 alias npacker="nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'"
+alias packerinstall="rm -rf $HOME/.local/share/nvim \
+  && rm -rf $HOME/dotfiles/config/nvim/plugin/packer_compiled.lua \
+  && git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+        ~/.local/share/nvim/site/pack/packer/start/packer.nvim"
 
 # tmux
 alias ta='tmux a -t'
