5d9a0c1c054fb78b6dab794b04f47d48e6647234
Kanmii <maneptha@gmail.com>
2022-05-22 18:49:11 +0200

    to vscode snippet python script

---
+++ to_snippet_vscode.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import re
+
+boundary_space_re = re.compile(r'^\s+')
+
+lout = []
+
+for line in open("snippet_in.txt"):
+    line = line.strip("\n")
+    search = boundary_space_re.search(line)
+    rep = ''
+
+    if search:
+        for r in range(len(search.group()) - 1):
+            rep += r'  '  # one tab length
+
+    line = line.strip().replace('\\', '\\\\').replace(
+        '"', '\\"'
+    ).replace('$', '\\\\$')
+
+    lout.append('"' + rep + line + '"')
+
+body = '[\n' + ',\n'.join(lout) + '\n]'
+
+prefix = """
+{
+  "name": {
+    "prefix": "",
+    "body": %s,
+    "description": ""
+  }
+}
+""".strip()
+open("snippet_out.json", "w").write(prefix % body)
