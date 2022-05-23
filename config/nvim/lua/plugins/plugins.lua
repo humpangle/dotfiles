578ce760ecbca630f9f940bfaf1e9b5c8dfd62f2
Kanmii <maneptha@gmail.com>
2022-05-23 10:28:30 +0200

    vim key easymotion - use leader 1/2 for 2 char search
    Because I'm always accidentally hitting leader 1

--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -385,6 +385,7 @@ return packer.startup(function(use)
           let g:EasyMotion_smartcase = 1
 
           nmap <leader>2 <Plug>(easymotion-overwin-f2)
+          nmap <leader>1 <Plug>(easymotion-overwin-f2)
           " nmap s <Plug>(easymotion-overwin-f2)
       ]])
     end,
