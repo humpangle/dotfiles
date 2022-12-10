d4fd6dc45bae5e3e75a8335bd0af513772a82708
Kanmii <maneptha@gmail.com>
2022-12-10 21:45:07 +0100

    wsl-nameserver - rewrite /etc/wsl.conf

--- etc/wsl-nameserver.sh
+++ etc/wsl-nameserver.sh
@@ -2,6 +2,13 @@
 
 filename="/etc/resolv.conf"
 
-[[ -h "$filename" ]] && sudo unlink "$filename"
+if [[ -n "$1" ]]; then
+  sudo sed -i -e 's|generateResolvConf = false|generateResolvConf = true|' /etc/wsl.conf
 
+  exit 0
+fi
+
+sudo unlink "$filename" 2>/dev/null || true
+sudo rm -rf "$filename" 2>/dev/null || true
+sudo sed -i -e 's|generateResolvConf = true|generateResolvConf = false|' /etc/wsl.conf
 echo 'nameserver 1.1.1.1' | sudo tee "${filename}" 1>/dev/null
