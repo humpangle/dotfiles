e3f7bcfb966d2a48064d551c5ace653ec60e630a
Kanmii <maneptha@gmail.com>
2022-12-25 04:48:26 +0100

    Coc restart keymap should ensure Coc enabled

--- config/nvim/lua/plugins/coc.lua
+++ config/nvim/lua/plugins/coc.lua
@@ -174,7 +174,7 @@ nmap <leader>fp :Prettier<CR>
 command! -nargs=0 OR :call CocAction('runCommand', 'editor.action.organizeImport')
 nmap ,o :OR<CR>
 
-nnoremap <leader>rs :<C-u>CocRestart<cr>
+nnoremap <leader>rs :<C-u>CocRestart<cr>:CocEnable<cr>
 nnoremap <leader>rd :<C-u>CocDisable<cr>
 nmap <Leader>ch :CocSearch <Right>
 
