2085e61f1ddf990b3a146524de5ac4692923e238
Kanmii <maneptha@gmail.com>
2024-05-03 08:00:04 -0400

    Deleted script open-wsl-explorer.sh as it is never used.
    In fact it's function is already incorporated into function
    _open_wsl_explorer.

--- scripts/open-wsl-explorer.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck disable=2034,2209,2135,2155
-
-if [[ -n "$HAS_WSL2" ]]; then
-  path="$(wslpath -w "$1")" # Convert unix to windows path
-  /c/WINDOWS/explorer.exe "$path"
-fi
