c07ed46b4caaeb1e2cde1e889dbccb7cf37a14e4
Kanmii <maneptha@gmail.com>
2024-06-28 16:53:57 -0400

    Sync nvim clipboard with OS in termux.

--- _termux/.config/nvim/lua/settings.lua
+++ _termux/.config/nvim/lua/settings.lua
@@ -66,7 +66,7 @@ vim.opt.mouse = "a"
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
--- vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = "unnamedplus"
 -- vim.opt.clipboard = ""
 
 vim.opt.tabstop = 2
