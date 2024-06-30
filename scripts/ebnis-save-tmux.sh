ffddb6bab1a1808ae9c2783cebe13c334ff321be
Kanmii <maneptha@gmail.com>
2024-06-30 06:56:51 -0400

    Use function main in ebnis-save-tmux.sh.

--- scripts/ebnis-save-tmux.sh
+++ scripts/ebnis-save-tmux.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-function _ebnis-save-tmux {
+main() {
   # Do not invoke if there is no active tmux session.
   if ! tmux ls &>/dev/null; then
     return
   fi
 
-  # make-bash-history-unique
-
   "$HOME/.tmux/plugins/tmux-resurrect/scripts/save.sh" "$@" &>/dev/null
 }
 
-_ebnis-save-tmux "$@"
+main "$@"
