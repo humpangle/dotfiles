humpangle/dotfiles

commit 56d8e995020d8acc48d74b6a2ab77a48ead7d2a7

Use pcall in several places to fix error launching nvim in termux.

.config/nvim/lua/plugins/lsp.lua
@@ -217,12 +217,15 @@ return {
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
-      local lsp_extended_capabilities = vim.tbl_deep_extend(
-        "force",
-        lsp_capabilities,
-        -- Augment neovim LSP capabilities with those from nvim cmp.
-        require("cmp_nvim_lsp").default_capabilities()
-      )
+
+      local lsp_extended_capabilities = lsp_capabilities
+
+      -- Augment neovim LSP capabilities with those from nvim cmp.
+      local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      if cmp_nvim_lsp_ok then
+        lsp_extended_capabilities =
+          vim.tbl_deep_extend("force", lsp_capabilities, cmp_nvim_lsp)
+      end
 
       local elixir_lsp_filetypes = {
         "elixir",
@@ -405,7 +408,7 @@ return {
               server.capabilities or {}
             )
 
-            lspconfig[server_name].setup(server)
+            pcall(lspconfig[server_name].setup, server)
           end,
         },
       })

.config/nvim/lua/plugins/lightline-utils.lua
@@ -28,7 +28,13 @@ local function abbreviate_path(file_path)
 end
 
 local get_yaml_schema = function()
-  local yaml_schema = require("plugins.yaml_lsp").get_yaml_schema()
+  local ok, yaml_lsp = pcall(require, "yaml_lsp")
+
+  if not ok then
+    return ""
+  end
+
+  local yaml_schema = yaml_lsp.get_yaml_schema()
 
   if yaml_schema ~= "" then
     yaml_schema = " " .. yaml_schema

commit c2621d074e655fad1a4a04d3d229d81926a15299

Fix error introduced in 56d8e99 -  use pcall in several places to fix error launching nvim in termux.

.config/nvim/lua/plugins/lsp.lua
@@ -223,8 +223,11 @@ return {
       -- Augment neovim LSP capabilities with those from nvim cmp.
       local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
       if cmp_nvim_lsp_ok then
-        lsp_extended_capabilities =
-          vim.tbl_deep_extend("force", lsp_capabilities, cmp_nvim_lsp)
+        lsp_extended_capabilities = vim.tbl_deep_extend(
+          "force",
+          lsp_capabilities,
+          cmp_nvim_lsp.default_capabilities()
+        )
       end
 
       local elixir_lsp_filetypes = {