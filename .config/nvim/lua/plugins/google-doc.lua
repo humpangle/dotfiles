45d84e21822b07501da911196484653814e5cb1d
Kanmii <maneptha@gmail.com>
2024-08-20 07:43:25 -0400

    Use callmiy/gdoc.nvim

--- .config/nvim/lua/plugins/google-doc.lua
+++ .config/nvim/lua/plugins/google-doc.lua
@@ -10,8 +10,9 @@ return {
   build = "./install.py",
   init = function()
     local credentials_directory = os.getenv("GOOGLE_DOC_VIM_DIR")
-    vim.g.path_to_creds = credentials_directory .. "/google-doc-credentials.json"
+    vim.g.path_to_creds = credentials_directory
+      .. "/google-doc-credentials.json"
     vim.g.token_directory = credentials_directory
-    vim.g.gdoc_file_path = "~/projects/google-doc-vim/"
+    vim.g.gdoc_file_path = "~/projects/google-doc/.vim/"
   end,
 }
