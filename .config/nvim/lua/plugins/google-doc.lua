bb2a7931b5569cac91c03129d7f5ca81382afbc0
Kanmii <maneptha@gmail.com>
2024-08-21 20:04:11 -0400

    Use github repo for gdoc.vim plugin

--- .config/nvim/lua/plugins/google-doc.lua
+++ .config/nvim/lua/plugins/google-doc.lua
@@ -4,17 +4,14 @@ if plugin_enabled.has_vscode() then
   return {}
 end
 
-local plugin_dir = os.getenv("GOOGLE_DOC_PLUGIN_DIR")
-
 return {
-  plugin_dir,
-  dir = "/Users/kanmii/projects/nvim/gdoc.vim",
-  build = "./install.py",
+  "callmiy/gdoc.vim",
   branch = "main",
+  build = "./install.py",
   init = function()
     local credentials_directory = os.getenv("GOOGLE_DOC_VIM_DIR")
     vim.g.path_to_creds = credentials_directory .. "/google-doc-credentials.json"
     vim.g.token_directory = credentials_directory
-    vim.g.gdoc_file_path = "~/projects/google-doc/.vim/"
+    vim.g.gdoc_file_path = "~/projects/google-doc-vim/"
   end,
 }
