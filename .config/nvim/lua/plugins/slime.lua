de4849e18a1a9bd48ddc2a7c41baba514d5eaa52
Kanmii <maneptha@gmail.com>
2024-09-16 12:50:22 -0400

    Rename slime text file prefix to s0 to conserve screen realestate.

--- .config/nvim/lua/plugins/slime.lua
+++ .config/nvim/lua/plugins/slime.lua
@@ -114,7 +114,7 @@ vim.api.nvim_create_user_command("Slime0", function(opts)
     return
   end
 
-  local name_affix = "~slime" .. timestamp
+  local name_affix = "s0" .. timestamp
   local filename = slime_dir .. "/" .. name_affix
 
   local file = io.open(filename, "w")
