fceae58cfe2b593c788f9b4264cf167d3a85b6c6
Kanmii <maneptha@gmail.com>
2024-10-15 06:41:19 -0400

    Add gp.nvim for chatgpt and other GPTs

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -462,4 +462,17 @@ return {
       },
     },
   },
+
+  -- OS dependencies `sudo apt-get install sox libsox-fmt-mp3`
+  {
+    "robitx/gp.nvim",
+    config = function()
+      local conf = {
+        -- For customization, refer to Install > Configuration in the Documentation/Readme
+      }
+      require("gp").setup(conf)
+
+      -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
+    end,
+  },
 }
