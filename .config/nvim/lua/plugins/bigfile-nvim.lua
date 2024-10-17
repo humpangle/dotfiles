5e0ab5e057c8808b1627db4c6e3fefba30e19c16
Kanmii <maneptha@gmail.com>
2024-10-17 05:27:39 -0400

    conditionally enable bigfile-nvim

--- .config/nvim/lua/plugins/bigfile-nvim.lua
+++ .config/nvim/lua/plugins/bigfile-nvim.lua
@@ -1,8 +1,11 @@
+local plugin_enabled = require("plugins/plugin_enabled")
+
 -- Improve performance of editing big files
 return {
   -- https://github.com/LunarVim/bigfile.nvim
   "LunarVim/bigfile.nvim",
   event = "BufReadPre",
+  enabled = plugin_enabled.has_big_file_nvim(),
   opts = {
     filesize = 2, -- size of the file in MiB, the plugin round file sizes to the closest MiB
     pattern = { "*" }, -- autocmd pattern or function see <### Overriding the detection of big files>
--- .config/nvim/lua/plugins/plugin_enabled.lua
+++ .config/nvim/lua/plugins/plugin_enabled.lua
@@ -103,4 +103,12 @@ function M.has_lua_json5()
   return true
 end
 
+function M.has_big_file_nvim()
+  if M.has_vscode() then
+    return false
+  end
+
+  return true
+end
+
 return M
