0045d0da338634e03824d3836993c4096b2b9ff6
Kanmii <maneptha@gmail.com>
2024-10-18 03:04:15 -0400

    code format nvim dadbod-ui.lua

--- .config/nvim/lua/plugins/dadbod-ui.lua
+++ .config/nvim/lua/plugins/dadbod-ui.lua
@@ -1,77 +1,77 @@
-  -- Database management
-  -- https://alpha2phi.medium.com/vim-neovim-managing-databases-d253faf4a0cd
+-- Database management
+-- https://alpha2phi.medium.com/vim-neovim-managing-databases-d253faf4a0cd
 
 local plugin_enabled = require("plugins/plugin_enabled")
 local utils = require("utils")
 local keymap = utils.map_key
 
 return {
-    "kristijanhusak/vim-dadbod-ui",
-    enabled = not plugin_enabled.has_vscode(),
-    dependencies = {
-      {
-        "tpope/vim-dadbod",
-        lazy = true,
-      },
-      -- Optional
-      {
-        "kristijanhusak/vim-dadbod-completion",
-        ft = { "sql", "mysql", "plsql" },
-        lazy = true,
-      },
+  "kristijanhusak/vim-dadbod-ui",
+  enabled = not plugin_enabled.has_vscode(),
+  dependencies = {
+    {
+      "tpope/vim-dadbod",
+      lazy = true,
     },
-    cmd = {
-      "DBUI",
-      "DBUIToggle",
-      "DBUIAddConnection",
-      "DBUIFindBuffer",
+    -- Optional
+    {
+      "kristijanhusak/vim-dadbod-completion",
+      ft = { "sql", "mysql", "plsql" },
+      lazy = true,
     },
-    init = function()
-      -- postgres — postgresql://user1:userpwd@localhost:5432/testdb
-      -- mysql — mysql://user1:userpwd@127.0.0.1:3306/testdb
-      -- sqlite - sqlite:path-to-sqlite-database
+  },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+  init = function()
+    -- postgres — postgresql://user1:userpwd@localhost:5432/testdb
+    -- mysql — mysql://user1:userpwd@127.0.0.1:3306/testdb
+    -- sqlite - sqlite:path-to-sqlite-database
 
-      -- :w = execute query in current DBUI buffer
+    -- :w = execute query in current DBUI buffer
 
-      -- Typical entry in ~/.local/share/db_ui/connections.json:
-      -- [
-      --   {
-      --     "url": "postgresql://username:password@127.0.0.1:5432/db_name",
-      --     "name": "folder_name"
-      --   },
-      --   {
-      --     "url": "mysql://username:password@127.0.0.1:5432/db_name",
-      --     "name": "folder_name"
-      --   }
-      -- ]
-      --
-      -- Create ~/.local/share/db_ui/folder_name/*.sql and write your sql queries.
+    -- Typical entry in ~/.local/share/db_ui/connections.json:
+    -- [
+    --   {
+    --     "url": "postgresql://username:password@127.0.0.1:5432/db_name",
+    --     "name": "folder_name"
+    --   },
+    --   {
+    --     "url": "mysql://username:password@127.0.0.1:5432/db_name",
+    --     "name": "folder_name"
+    --   }
+    -- ]
+    --
+    -- Create ~/.local/share/db_ui/folder_name/*.sql and write your sql queries.
 
-      vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_use_nerd_fonts = 1
 
-      keymap(
-        "n",
-        "<leader>dbi",
-        ":tab new<CR>:DBUI<CR><C-w>o<bar><C-w>v<bar>:e ~/.local/share/db_ui/connections.json<CR>",
-        {
-          noremap = true,
-          desc = "Initiialize DBUI",
-        }
-      )
-
-      keymap("n", "<leader>dbf", ":DBUIFindBuffer<CR>", {
+    keymap(
+      "n",
+      "<leader>dbi",
+      ":tab new<CR>:DBUI<CR><C-w>o<bar><C-w>v<bar>:e ~/.local/share/db_ui/connections.json<CR>",
+      {
         noremap = true,
-        desc = "DBUIFindBuffer",
-      })
+        desc = "Initiialize DBUI",
+      }
+    )
 
-      keymap("n", "<leader>dbr", ":DBUIRenameBuffer<CR>", {
-        noremap = true,
-        desc = "DBUIRenameBuffer",
-      })
+    keymap("n", "<leader>dbf", ":DBUIFindBuffer<CR>", {
+      noremap = true,
+      desc = "DBUIFindBuffer",
+    })
 
-      keymap("n", "<leader>dbl", ":DBUILastQueryInfo<CR>", {
-        noremap = true,
-        desc = "DBUILastQueryInfo",
-      })
-    end,
-  }
+    keymap("n", "<leader>dbr", ":DBUIRenameBuffer<CR>", {
+      noremap = true,
+      desc = "DBUIRenameBuffer",
+    })
+
+    keymap("n", "<leader>dbl", ":DBUILastQueryInfo<CR>", {
+      noremap = true,
+      desc = "DBUILastQueryInfo",
+    })
+  end,
+}
