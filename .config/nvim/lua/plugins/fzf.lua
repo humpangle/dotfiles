4f9822756723046f8cd932562f7d02b70eae630a
Kanmii <maneptha@gmail.com>
2024-10-19 06:04:33 -0400

    nvim fzf.lua: rename variable keymap -> map_key

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -6,13 +6,13 @@ end
 
 local utils = require("utils")
 
-local keymap = utils.map_key
+local map_key = utils.map_key
 
 -- Search file from root directory
-keymap("n", "<leader>fW", ":Files!<CR>", { noremap = true })
+map_key("n", "<leader>fW", ":Files!<CR>", { noremap = true })
 
 -- Search file from current directory
-keymap(
+map_key(
   "n",
   "<Leader>f.",
   ":Files! " .. vim.fn.expand("%:h") .. "/<CR>",
@@ -20,48 +20,48 @@ keymap(
 )
 
 -- Find open buffers
-keymap("n", "<Leader>fb", function()
+map_key("n", "<Leader>fb", function()
   utils.handle_cant_re_enter_normal_mode_from_terminal_mode(function()
     vim.cmd("Buffers!")
   end)
 end, { noremap = true })
 
 -- Search buffers history
-keymap("n", "<Leader>fh", ":History!<CR>", { noremap = true })
+map_key("n", "<Leader>fh", ":History!<CR>", { noremap = true })
 
 -- Search for text in current buffer
-keymap("n", "<Leader>fl", ":BLines!<CR>", { noremap = true })
+map_key("n", "<Leader>fl", ":BLines!<CR>", { noremap = true })
 
 -- Search for text in loaded buffers
-keymap("n", "<Leader>L", ":Lines!<CR>", { noremap = true })
+map_key("n", "<Leader>L", ":Lines!<CR>", { noremap = true })
 
 -- Search for marks
-keymap("n", "<Leader>fm", ":Marks!<CR>", { noremap = true })
+map_key("n", "<Leader>fm", ":Marks!<CR>", { noremap = true })
 
 -- Find filetypes
-keymap("n", "<leader>ft", ":Filetypes!<CR>", { noremap = true })
+map_key("n", "<leader>ft", ":Filetypes!<CR>", { noremap = true })
 
 -- Find windows
-keymap("n", "<leader>fw", ":Windows!<CR>", { noremap = true })
+map_key("n", "<leader>fw", ":Windows!<CR>", { noremap = true })
 
 -- Find color schemes
-keymap("n", "<leader>fs", ":Colors!<CR>", { noremap = true })
+map_key("n", "<leader>fs", ":Colors!<CR>", { noremap = true })
 
 -- Search commands: user defined, plugin defined, or native commands
-keymap("n", "<Leader>C", ":Commands!<CR>", { noremap = true })
+map_key("n", "<Leader>C", ":Commands!<CR>", { noremap = true })
 
 -- Search key mappings - find already mapped before defining new mappings
-keymap("n", "<Leader>M", ":Maps!<CR>", { noremap = true })
+map_key("n", "<Leader>M", ":Maps!<CR>", { noremap = true })
 
 -- Search in project - do not match filenames
-keymap("n", "<Leader>/", ":RgNf!<CR>", { noremap = true })
+map_key("n", "<Leader>/", ":RgNf!<CR>", { noremap = true })
 
 --  GIT
 -- Files managed by git
-keymap("n", "<Leader>fg", ":GFiles!<CR>", { noremap = true })
+map_key("n", "<Leader>fg", ":GFiles!<CR>", { noremap = true })
 
 -- Search file from current directory with Git
-keymap("n", "<Leader>f,", function()
+map_key("n", "<Leader>f,", function()
   vim.cmd("GFiles! " .. vim.fn.expand("%:h") .. "/")
 end, { noremap = true })
 
@@ -69,7 +69,7 @@ end, { noremap = true })
 -- keymap('n', '<leader>cm', ':Commits!<CR>', { noremap = true})
 
 -- Git commits for the current buffer
-keymap("n", "<leader>%c", ":BCommits!<CR>", { noremap = true })
+map_key("n", "<leader>%c", ":BCommits!<CR>", { noremap = true })
 
 -- fzf-checkout find git branch:
 --   checkout = <CR>
@@ -77,22 +77,22 @@ keymap("n", "<leader>%c", ":BCommits!<CR>", { noremap = true })
 --   delete = <C-d>
 --   merge = <C-e>
 --   track remote = <a-cr>
-keymap("n", "<leader>cb", ":GBranches!<CR>", { noremap = true })
+map_key("n", "<leader>cb", ":GBranches!<CR>", { noremap = true })
 
 -- Search in project - match file names first
-keymap("n", ",/", ":Rg!<CR>", { noremap = true })
+map_key("n", ",/", ":Rg!<CR>", { noremap = true })
 
 -- Snippets (commented out since Snippets might not be directly related to fzf.vim or might need a specific plugin)
 -- keymap('n', '<leader>sn', ':Snippets<CR>', { noremap = true})
 
 -- Vim’s :help documentation
-keymap("n", "<Leader>H", ":Helptags!<CR>", { noremap = true })
+map_key("n", "<Leader>H", ":Helptags!<CR>", { noremap = true })
 
 -- Fzf quickfix list
-keymap("n", "<leader>fq", ":FzfQF!<CR>", { noremap = true })
+map_key("n", "<leader>fq", ":FzfQF!<CR>", { noremap = true })
 
 -- Fzf location list
-keymap("n", "<leader>FL", ":LocList!<CR>", { noremap = true })
+map_key("n", "<leader>FL", ":LocList!<CR>", { noremap = true })
 
 -- Translation to lua does not work.
 vim.cmd([[
