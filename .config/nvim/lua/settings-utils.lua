a6c83c80c00e6add164745a9a4651720393224bf
Kanmii <maneptha@gmail.com>
2024-10-19 06:52:19 -0400

    document nvim function RedirMessages

--- .config/nvim/lua/settings-utils.lua
+++ .config/nvim/lua/settings-utils.lua
@@ -1,13 +1,27 @@
 local utils = {}
 
-function utils.RedirMessages(msgcmd, destcmd)
-  --[[ Examples of usage:
-        RedirMessages('registers', '')
-        RedirMessages('registers', 'new')
-        RedirMessages('registers', 'vnew')
-        RedirMessages('registers', 'tabnew')
-  -- ]]
+--[[
+msgcmd: a vim command that will output a string of messages.
+destcmd: is the place to send the output. If destcmd not specified, output will be sent to current buffer.
+  Other values for destcmd are:
+    new - horizontal split
+    vsplit - vertical split
+    tabnew - new tabnew
 
+Examples of usage:
+  RedirMessages('registers', '')
+  RedirMessages('registers', 'new')
+  RedirMessages('registers', 'vnew')
+  RedirMessages('registers', 'tabnew')
+
+Create commands to make RedirMessages() easier to use interactively.
+  Here are some examples of their use:
+    :Bmessage registers
+    :Wmessage ls
+    :VMessage !ls -alh
+    :Tmessage echo "Key mappings for Control+A:" | map <C-A>
+-- ]]
+function utils.RedirMessages(msgcmd, destcmd)
   -- Redirect command output to a variable in Lua
   local message = vim.fn.execute(msgcmd)
 
@@ -22,12 +36,5 @@ function utils.RedirMessages(msgcmd, destcmd)
   local current_line = vim.api.nvim_win_get_cursor(0)[1]
   vim.api.nvim_buf_set_lines(0, current_line, current_line, false, lines)
 end
-
---[[ Create commands to make RedirMessages() easier to use interactively.
-  Here are some examples of their use:
-      :BufMessage registers
-      :WinMessage ls
-      :TabMessage echo "Key mappings for Control+A:" | map <C-A>
---]]
 
 return utils
