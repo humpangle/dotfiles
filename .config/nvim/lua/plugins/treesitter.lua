9ea21ae6a0edd6c9b0ddb4b4a97231d84a27ae4b
Kanmii <maneptha@gmail.com>
2024-11-13 19:54:14 -0500

    move nvim-treesitter config from opt -  to prep for further configuration

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -12,67 +12,66 @@ return {
       require("plugins/treesitter-textobjects"),
     },
     build = ":TSUpdate",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "css",
-        "dockerfile",
-        "eex",
-        "elixir",
-        "heex",
-        "erlang",
-        "gitignore",
-        "go",
-        "helm",
-        "html",
-        "ini",
-        "jsdoc",
-        "json5",
-        "jsonc", -- required by "folke/neoconf.nvim"
-        "lua",
-        "luadoc",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "regex",
-        "scss",
-        "sql",
-        "ssh_config",
-        "terraform",
-        "tmux",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "yaml",
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = {
-          "ruby",
-        },
-        disable = {
-          "gitcommit", -- treesitter highlighting does not work well inside buffer of this type.
-        },
-      },
-      indent = {
-        enable = true,
-        disable = {
-          "ruby",
-        },
-      },
-    },
-    config = function(_, opts)
+    config = function()
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
       ---@diagnostic disable-next-line: missing-fields
-      require("nvim-treesitter.configs").setup(opts)
+      require("nvim-treesitter.configs").setup({
+        highlight = {
+          enable = true,
+          -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+          --  If you are experiencing weird indenting issues, add the language to
+          --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+          additional_vim_regex_highlighting = {
+            "ruby",
+          },
+          disable = {
+            "gitcommit", -- treesitter highlighting does not work well inside buffer of this type.
+          },
+        },
+        ensure_installed = {
+          "bash",
+          "css",
+          "dockerfile",
+          "eex",
+          "elixir",
+          "heex",
+          "erlang",
+          "gitignore",
+          "go",
+          "helm",
+          "html",
+          "ini",
+          "jsdoc",
+          "json5",
+          "jsonc", -- required by "folke/neoconf.nvim"
+          "lua",
+          "luadoc",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "regex",
+          "scss",
+          "sql",
+          "ssh_config",
+          "terraform",
+          "tmux",
+          "toml",
+          "tsx",
+          "typescript",
+          "vim",
+          "vimdoc",
+          "yaml",
+        },
+        -- Autoinstall languages that are not installed
+        auto_install = true,
+        indent = {
+          enable = true,
+          disable = {
+            "ruby",
+          },
+        },
+      })
 
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
