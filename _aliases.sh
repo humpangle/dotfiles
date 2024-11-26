b4fe6ffce769c2133b4680975d66d44a9fdd649b
Kanmii <maneptha@gmail.com>
2024-11-25 20:50:59 -0500

    change 'which code' to 'command -v'

--- _aliases.sh
+++ _aliases.sh
@@ -407,7 +407,7 @@ alias _pm----help='provison machine'
 if _has_wsl; then
   export VSCODE_BINARY="/c/Users/$USERNAME/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code"
 else
-  export VSCODE_BINARY="$(which code)"
+  export VSCODE_BINARY="$(command -v code)"
 fi
 
 _____c_help() {
