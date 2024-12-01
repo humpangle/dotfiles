bd90debd3b938eb13fc5bcfb65165477e345cdd9
Kanmii <maneptha@gmail.com>
2024-12-01 01:30:11 -0500

    neotest python pytest args - increase verbosity

--- .config/nvim/lua/plugins/neotest/init.lua
+++ .config/nvim/lua/plugins/neotest/init.lua
@@ -43,7 +43,7 @@ return {
         },
       },
       init = function()
-        vim.g.__ebnis_neotest_python_args = { "--disable-warnings" }
+        vim.g.__ebnis_neotest_python_args = { "--disable-warnings", "-vvv" }
 
         vim.api.nvim_create_user_command("NeoPyArgs", function(opts)
           local count = opts.fargs[1]
