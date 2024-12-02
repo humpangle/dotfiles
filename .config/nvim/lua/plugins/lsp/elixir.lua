5653cd35be015912b5cdf09d39575b9bea768e51
Kanmii <maneptha@gmail.com>
2024-12-01 22:04:04 -0500

    ensure only one of elixirls/lexical nvim lsp runs at a time

--- .config/nvim/lua/plugins/lsp/elixir.lua
+++ .config/nvim/lua/plugins/lsp/elixir.lua
@@ -6,31 +6,39 @@ end
 
 local utils = require("utils")
 
-local none_existing_vim_filetype = { "1" } -- hopefully a filetype that does not exist.
+-- If a lsp client is disabled, nvim can still suggest it (although not started) - we will see:
+--    Other clients that match the filetype: elixir
+--  The below will silence this message (pre-mature optmization ??)
+local none_existing_vim_filetype = { "1" }
 
 local M = {}
+
+-- We want just one of elixirls/lexical running at a time. If elixirls is enabled, lexical is prevented from running.
 
 if plugin_enabled.lexical() then
   -- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/lua/mason-lspconfig/server_configurations/lexical/init.lua
   M.lexical = {
+    -- I have a  global ELIXIR_LEXICAL_BIN in .bashrc and then project specific in the workspace root.
     cmd = {
-      -- I have a  global ELIXIR_LEXICAL_BIN in .bashrc and then project specific in the workspace root.
-      os.getenv("ELIXIR_LEXICAL_BIN") or "lexical",
+      utils.get_os_env_or_nil("ELIXIR_LEXICAL_BIN") or "lexical",
     },
+  }
+
+  M.elixirls = {
+    autostart = false,
     filetypes = none_existing_vim_filetype,
   }
-else
+elseif plugin_enabled.elixir_ls() then
   M.elixirls = {
     cmd = {
       -- I have a  global ELIXIR_LS_BIN in .bashrc and then project specific in the workspace root.
       utils.get_os_env_or_nil("ELIXIR_LS_BIN") or "elixir-ls",
     },
-    filetypes = {
-      "elixir",
-      "eelixir",
-      "heex",
-      "surface",
-    },
+  }
+
+  M.lexical = {
+    autostart = false,
+    filetypes = none_existing_vim_filetype,
   }
 end
 
