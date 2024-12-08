f69ac44a2300cab3e2a024461d022d0e33c61a0d
Kanmii <maneptha@gmail.com>
2024-12-08 08:02:35 -0500

    nvim lsp install sqls

--- .config/nvim/lua/plugins/lsp/mason-ensure-installed.lua
+++ .config/nvim/lua/plugins/lsp/mason-ensure-installed.lua
@@ -1,3 +1,14 @@
 return {
   ["sql-formatter"] = {},
+
+  sqls = {
+    filetypes = {
+      "sql",
+      "mysql",
+      "markdown.sql",
+      "markdown.mysql",
+      "sql.markdown",
+      "mysql.markdown",
+    },
+  },
 }
