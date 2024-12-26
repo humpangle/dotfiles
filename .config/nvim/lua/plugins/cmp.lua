9c103387b74e2cbe9b55f4380a7a50d2c070acb3
Kanmii <maneptha@gmail.com>
2024-12-26 06:06:26 -0500

    code format cmp.lua

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -151,8 +151,8 @@ return {
         -- This step is not supported in many windows environments.
         -- Remove the below condition to re-enable on windows.
         if
-            vim.fn.has("win32") == 1
-            or vim.fn.executable("make") == 0
+          vim.fn.has("win32") == 1
+          or vim.fn.executable("make") == 0
         then
           return
         end
