008999ea363fee785b74b781eee37bad833d8135
Kanmii <maneptha@gmail.com>
2025-01-20 07:01:32 -0500

    source ___scratch-env script as we may have more than environment variable exports

--- .bashrc-template--env-scratch-example.sh
+++ .bashrc-template--env-scratch-example.sh
@@ -3,4 +3,13 @@
 
 # Example common entries in `.___scratch-env.sh`
 
-NVIM_GO_TO_FILE_GF_STRIP_PREFIX=backend/api.patients/
+# Source python virtualenv helper script
+# shellcheck disable=1091
+source "$DOTFILE_ROOT/_pv.sh"
+
+# Allows nvim gf keymap to work properly, especially, in git diff views where the root of the project is not git root
+export NVIM_GO_TO_FILE_GF_STRIP_PREFIX=backend/api.patients/
+
+# For python projects - invoke virtualenv
+_pv -d
+_pv
