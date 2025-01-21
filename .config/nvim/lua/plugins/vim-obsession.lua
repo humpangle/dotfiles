5a4f7ce82465b3e36fd118697b0fcd32e39e7449
Kanmii <maneptha@gmail.com>
2025-01-21 05:25:12 -0500

    nvim keymap Obsession - start session if session file does not exist

--- .config/nvim/lua/plugins/vim-obsession.lua
+++ .config/nvim/lua/plugins/vim-obsession.lua
@@ -7,6 +7,11 @@ if plugin_enabled.has_vscode() then
 end
 
 -- Manage Vim Sessions Manually
+
+local file_session_vim_exists = function()
+  return vim.fn.glob("session.vim") ~= ""
+end
+
 return {
   "tpope/vim-obsession",
   init = function()
@@ -14,7 +19,11 @@ return {
       local count = vim.v.count
 
       if count == 0 then
-        utils.write_to_command_mode("so session.vim")
+        if file_session_vim_exists() then
+          utils.write_to_command_mode("so session.vim")
+        else
+          utils.write_to_command_mode("Obsession session.vim")
+        end
       elseif count == 1 then
         utils.write_to_command_mode("so session")
       elseif count == 2 then
