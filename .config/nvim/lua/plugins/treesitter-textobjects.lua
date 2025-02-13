516667cb7a88ff231e2204cc8fe899a8230a0c6c
Kanmii <maneptha@gmail.com>
2025-02-13 05:48:52 -0500

    nvim-treesitter-textobjects - including surrounding newline in textobjects
    This allows python decorators to be included in, for example, function
    textobjects

--- .config/nvim/lua/plugins/treesitter-textobjects.lua
+++ .config/nvim/lua/plugins/treesitter-textobjects.lua
@@ -50,7 +50,7 @@ return {
           -- * query_string: eg '@function.inner'
           -- * selection_mode: eg 'v'
           -- and should return true or false
-          include_surrounding_whitespace = false,
+          include_surrounding_whitespace = true,
         },
 
         -- Text objects: swap
