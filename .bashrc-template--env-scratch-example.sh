b4899372c11553e6354168e85314b3a109b548ee
Kanmii <humpangle@gmail.com>
2025-02-15 14:09:39 -0500

    tmux multi popup

--- .bashrc-template--env-scratch-example.sh
+++ .bashrc-template--env-scratch-example.sh
@@ -3,13 +3,16 @@
 
 # Example common entries in `.___scratch-env.sh`
 
-# Source python virtualenv helper script
-# shellcheck disable=1091
-source "$DOTFILE_ROOT/_pv.sh"
-
 # Allows nvim gf keymap to work properly, especially, in git diff views where the root of the project is not git root
 export NVIM_GO_TO_FILE_GF_STRIP_PREFIX=backend/api.patients/
 
+# Source python virtualenv helper script
+# shellcheck disable=1091
+source "$DOTFILE_ROOT/_pv.sh"
 # For python projects - invoke virtualenv
 _pv -d
 _pv
+
+# Use tmux for such things as running tests and one-off commands.
+export TMUX_POPUP_SESSION_NAME_AND_PATH="some-session-name:/some-path"
+export TMUX_POPUP_SESSION_NAME_AND_PATH="some-session-name"
