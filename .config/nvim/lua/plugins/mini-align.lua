b7da196c79c1a060151c7626162e8633abc7bd06
Kanmii <maneptha@gmail.com>
2025-04-04 09:41:45 -0400

    mini-align.nvim - change keymap prefix to leader/al

--- .config/nvim/lua/plugins/mini-align.lua
+++ .config/nvim/lua/plugins/mini-align.lua
@@ -2,6 +2,14 @@ return {
   "echasnovski/mini.align",
   version = "*",
   config = function()
-    require("mini.align").setup()
+    require("mini.align").setup({
+      -- Module mappings. Use `''` (empty string) to disable one.
+      mappings = {
+        start = "<leader>al0",
+        start_with_preview = "<leader>al1",
+      },
+    })
+    -- Usage:
+    -- Start with `<leader>alip`
   end,
 }
