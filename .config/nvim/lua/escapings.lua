cc91b94beb246d5cf793e8a9706f5c719ce68ce4
Kanmii <maneptha@gmail.com>
2025-06-23 00:17:03 -0400

    Replace DeAnsify regex with ansifilter command
    Changed the DeAnsify user command to use the external ansifilter
    tool instead of regex substitution for more robust ANSI escape
    sequence removal. The implementation writes buffer content to a
    temporary file, processes it with ansifilter, and replaces the
    buffer with the filtered output.

--- .config/nvim/lua/escapings.lua
+++ .config/nvim/lua/escapings.lua
@@ -35,9 +35,47 @@ vim.api.nvim_create_user_command("FilenameSanitize", function()
 end, {})
 
 vim.api.nvim_create_user_command("DeAnsify", function()
-  -- ANSI escape sequences
-  vim.cmd("%s/\x1b[[;0-9]*m//g")
+  -- Use ansifilter to remove ANSI escape sequences
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local content = table.concat(lines, "\n")
 
-  -- Carriage returns (Windows line endings)
-  -- vim.cmd([[%s/\r//g]])
-end, { desc = "Remove ANSI color codes and Windows line endings" })
+  -- Create a temporary file with the buffer content
+  local tmpfile = vim.fn.tempname()
+  local outfile = vim.fn.tempname()
+
+  -- Write content to temporary file
+  local file = io.open(tmpfile, "w")
+  if file then
+    file:write(content)
+    file:close()
+
+    -- Run ansifilter command
+    local cmd = string.format("ansifilter --text %s > %s", vim.fn.shellescape(tmpfile), vim.fn.shellescape(outfile))
+    local result = vim.fn.system(cmd)
+
+    -- Read the filtered content
+    local filtered_file = io.open(outfile, "r")
+    if filtered_file then
+      local filtered_content = filtered_file:read("*all")
+      filtered_file:close()
+
+      -- Replace buffer content with filtered text
+      local filtered_lines = vim.split(filtered_content, "\n", { plain = true, trimempty = false })
+      -- Remove the last empty line if it exists (common with file reading)
+      if filtered_lines[#filtered_lines] == "" then
+        table.remove(filtered_lines)
+      end
+      vim.api.nvim_buf_set_lines(0, 0, -1, false, filtered_lines)
+
+      vim.notify("Buffer content filtered with ansifilter")
+    else
+      vim.notify("Failed to read filtered output", vim.log.levels.ERROR)
+    end
+
+    -- Clean up temporary files
+    vim.fn.delete(tmpfile)
+    vim.fn.delete(outfile)
+  else
+    vim.notify("Failed to create temporary file", vim.log.levels.ERROR)
+  end
+end, { desc = "Remove ANSI color codes using ansifilter command" })
