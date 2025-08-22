8ba8bfc97a91687946b5d2bb820649efb0dabac0
Kanmii <maneptha@gmail.com>
2025-08-22 00:21:14 -0400

    nvim gitsigns: show line blame by default and change delay to 600 (from 1000)

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -23,11 +23,11 @@ return {
     local gitsigns = require("gitsigns")
 
     gitsigns.setup({
-      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-        delay = 1000,
+        delay = 600,
         ignore_whitespace = false,
         virt_text_priority = 100,
         use_focus = true,
