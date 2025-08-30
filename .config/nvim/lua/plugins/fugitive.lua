2f17593eb04bec326af4e588d5a9b746ca03884f
Kanmii <maneptha@gmail.com>
2025-08-29 22:59:10 -0400

    nvim fugitive: rebase root mappings - fix description/count alignment

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -462,49 +462,49 @@ end, { noremap = true, desc = "Git config user name. 1=email 2=env defaults" })
 
 local git_rebase_options = {
   {
-    description = "Rebase                         1",
+    description = "Rebase                                                1",
     action = function()
       utils.write_to_command_mode("G rebase ")
     end,
     count = 1,
   },
   {
-    description = "Rebase Main                    11",
+    description = "Rebase Main                                           11",
     action = function()
       utils.write_to_command_mode("G rebase main")
     end,
     count = 11,
   },
   {
-    description = "Rebase Develop                 12",
+    description = "Rebase Develop                                        12",
     action = function()
       utils.write_to_command_mode("G rebase develop")
     end,
     count = 12,
   },
   {
-    description = "Rebase -i                      2",
+    description = "Rebase -i                                             2",
     action = function()
       utils.write_to_command_mode("G rebase -i ")
     end,
     count = 2,
   },
   {
-    description = "Rebase -i root                 21",
+    description = "Rebase -i root                                        21",
     action = function()
       utils.write_to_command_mode("G rebase -i --root")
     end,
     count = 21,
   },
   {
-    description = "Rebase -i HEAD~                22",
+    description = "Rebase -i HEAD~                                       22",
     action = function()
       utils.write_to_command_mode("G rebase -i HEAD~")
     end,
     count = 22,
   },
   {
-    description = "Rebase -i <cword> cursor       23",
+    description = "Rebase -i <cword> cursor                              23",
     action = function()
       utils.write_to_command_mode(
         "G rebase -i " .. vim.fn.expand(highlight_text_under_cursor())
@@ -513,14 +513,14 @@ local git_rebase_options = {
     count = 23,
   },
   {
-    description = "Reset soft HEAD~               3",
+    description = "Reset soft HEAD~                                      3",
     action = function()
       utils.write_to_command_mode("G reset --soft HEAD~")
     end,
     count = 3,
   },
   {
-    description = "Reset soft <cword> cursor      31",
+    description = "Reset soft <cword> cursor                             31",
     action = function()
       utils.write_to_command_mode(
         "G reset --soft "
@@ -530,21 +530,21 @@ local git_rebase_options = {
     count = 31,
   },
   {
-    description = "Reset soft                     32",
+    description = "Reset soft                                           32",
     action = function()
       utils.write_to_command_mode("G reset --soft ")
     end,
     count = 32,
   },
   {
-    description = "Reset hard HEAD~               4",
+    description = "Reset hard HEAD~                                     4",
     action = function()
       utils.write_to_command_mode("G reset --hard HEAD~")
     end,
     count = 4,
   },
   {
-    description = "Reset hard <cword> cursor      41",
+    description = "Reset hard <cword> cursor                            41",
     action = function()
       utils.write_to_command_mode(
         "G reset --hard "
@@ -554,28 +554,28 @@ local git_rebase_options = {
     count = 41,
   },
   {
-    description = "Merge main                     5",
+    description = "Merge main                                           5",
     action = function()
       utils.write_to_command_mode("G merge main")
     end,
     count = 5,
   },
   {
-    description = "Merge master                   51",
+    description = "Merge master                                         51",
     action = function()
       utils.write_to_command_mode("G merge master")
     end,
     count = 51,
   },
   {
-    description = "Merge develop                  52",
+    description = "Merge develop                                        52",
     action = function()
       utils.write_to_command_mode("G merge develop")
     end,
     count = 52,
   },
   {
-    description = "Merge                          53",
+    description = "Merge                                                53",
     action = function()
       utils.write_to_command_mode("G merge ")
     end,
