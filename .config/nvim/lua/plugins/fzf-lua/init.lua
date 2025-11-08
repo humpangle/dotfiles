02aca82e21ad3ecaaa4a6e2eb9fd9bb0d59c6265
Kanmii <maneptha@gmail.com>
2025-11-08 06:36:30 -0500

    nvim fzf-lua: shorten git branch delete label

--- .config/nvim/lua/plugins/fzf-lua/init.lua
+++ .config/nvim/lua/plugins/fzf-lua/init.lua
@@ -90,6 +90,7 @@ return {
       -- Register custom action label
       fzf_lua_core.ACTION_DEFINITIONS[fzf_lua_utils.git_branch_force_del] = { "Force Del" }
       fzf_lua_core.ACTION_DEFINITIONS[fzf_lua_actions.git_branch_add] = { "Add" }
+      fzf_lua_core.ACTION_DEFINITIONS[fzf_lua_actions.git_branch_del] = { "Del" }
 
       local opts = {}
 
