ad356ed5ff64325bc2d2591e7badcc593b75c0ec
Kanmii <maneptha@gmail.com>
2025-11-09 12:59:52 -0500

    nvim fzf-lua-utils: code format

--- .config/nvim/lua/plugins/fzf-lua/fzf-lua-utils.lua
+++ .config/nvim/lua/plugins/fzf-lua/fzf-lua-utils.lua
@@ -11,8 +11,7 @@ M.git_branch_force_del = function(selected, opts)
   ---@type string[]
   local cmd_del_branch = path.git_cwd({ "git", "branch", "-D" }, opts)
   ---@type string[]
-  local cmd_cur_branch =
-    path.git_cwd({ "git", "rev-parse", "--abbrev-ref", "HEAD" }, opts)
+  local cmd_cur_branch = path.git_cwd({ "git", "rev-parse", "--abbrev-ref", "HEAD" }, opts)
   local branch = selected[1]:match("^[%*+]*[%s]*[(]?([^%s)]+)")
   local cur_branch_result = utils.io_systemlist(cmd_cur_branch)
   local cur_branch = cur_branch_result[1]
@@ -20,10 +19,7 @@ M.git_branch_force_del = function(selected, opts)
     utils.warn("Cannot delete active branch '%s'", branch)
     return
   end
-  if
-    vim.fn.confirm("Force delete branch " .. branch .. "?", "&Yes\n&No")
-    == 1
-  then
+  if vim.fn.confirm("Force delete branch " .. branch .. "?", "&Yes\n&No") == 1 then
     table.insert(cmd_del_branch, branch)
     local output, rc = utils.io_systemlist(cmd_del_branch)
     if rc ~= 0 then
