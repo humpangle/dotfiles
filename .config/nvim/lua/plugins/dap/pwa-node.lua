e8f3188f5e3168258f8707f8de7d1d66f7b87789
Kanmii <maneptha@gmail.com>
2025-11-17 05:02:37 -0500

    WIP nvim dap: re-do pwa-node.lua

--- .config/nvim/lua/plugins/dap/pwa-node.lua
+++ .config/nvim/lua/plugins/dap/pwa-node.lua
@@ -1,112 +1,211 @@
--- Taken from https://banjocode.com/post/nvim/debug-node
+-- Taken from -- https://github.com/StevanFreeborn/nvim-config
+-- https://www.youtube.com/watch?v=DVG3m7rNFKc
 
 local dap = require("dap")
-local utils = require("utils")
 
-local function pick_script()
-  local pilot = require("package-pilot")
-
-  local current_dir = vim.fn.getcwd()
-  local package = pilot.find_package_file({ dir = current_dir })
-
-  if not package then
-    vim.notify("No package.json found", vim.log.levels.ERROR)
-    return require("dap").ABORT
+local get_user_input = function(opts)
+  return function()
+    opts = vim.tbl_extend("keep", opts or {}, {
+      prompt = "Enter URL: ",
+      default = "http://localhost:",
+    })
+    local co = coroutine.running()
+    return coroutine.create(function()
+      vim.ui.input(opts, function(userInput)
+        if userInput == nil or userInput == "" then
+          return
+        else
+          coroutine.resume(co, userInput)
+        end
+      end)
+    end)
   end
-
-  local scripts = pilot.get_all_scripts(package)
-
-  local label_fn = function(script)
-    return script
-  end
-
-  local co, ismain = coroutine.running()
-  local ui = require("dap.ui")
-  local pick = (co and not ismain) and ui.pick_one or ui.pick_one_sync
-  local result = pick(scripts, "Select script", label_fn)
-  return result or require("dap").ABORT
 end
 
-if not dap.adapters["pwa-node"] then
-  dap.adapters["pwa-node"] = {
+for _, adapter_type in ipairs({
+  "node",
+  "chrome",
+  "msedge",
+}) do
+  local pwa_type = "pwa-" .. adapter_type
+  dap.adapters[pwa_type] = {
     type = "server",
     host = "localhost",
     port = "${port}",
     executable = {
       command = "node",
       args = {
-        utils.mason_install_path
-          .. "/js-debug-adapter/js-debug/src/dapDebugServer.js",
+        vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
         "${port}",
       },
     },
   }
-end
 
-if not dap.adapters["node"] then
-  dap.adapters["node"] = function(cb, config)
-    if config.type == "node" then
-      config.type = "pwa-node"
-    end
-    local pwa_node_adapter = dap.adapters["pwa-node"]
-    if type(pwa_node_adapter) == "function" then
-      pwa_node_adapter(cb, config)
+  -- This allows us to handle launch.json configurations which specify type as "node" or "chrome" or "msedge" instead
+  -- of pwa-node/pwa-chrome etc
+  dap.adapters[adapter_type] = function(cb, config)
+    local native_adapter = dap.adapters[pwa_type]
+    config.type = pwa_type
+    if type(native_adapter) == "function" then
+      native_adapter(cb, config)
     else
-      cb(pwa_node_adapter)
+      cb(native_adapter)
     end
   end
 end
 
-local js_filetypes = {
+local sourceMapPathOverrides = {
+
+  -- webpack-internal:// from your screenshot
+  ["webpack-internal:///src/*"] = "${workspaceFolder}/src/*",
+  ["webpack-internal:///./src/*"] = "${workspaceFolder}/src/*",
+  ["webpack-internal:///*"] = "${workspaceFolder}/*",
+
+  -- Webpack-style paths
+  ["webpack:///src/*"] = "${workspaceFolder}/src/*",
+  ["webpack:///./src/*"] = "${workspaceFolder}/src/*",
+  ["webpack:///./*"] = "${workspaceFolder}/*",
+  ["webpack:///*"] = "${workspaceFolder}/*",
+
+  ["webpack://alayacare/accloud-mfe-scheduling/src/*"] = "${workspaceFolder}/src/*",
+  ["webpack://alayacare/accloud-mfe-scheduling/*"] = "${workspaceFolder}/*",
+
+  -- Docker path inside container -> host path
+  ["/app/*"] = "${workspaceFolder}/*",
+
+  -- Some Vue setups use "vite:///" or similar; harmless if unused
+  ["vite:///*"] = "${workspaceFolder}/*",
+}
+-- sourceMapPathOverrides = {
+--   ["*webpack://**/src"] = "${workspaceFolder/src}",
+-- }
+
+for _, language in ipairs({
   "typescript",
   "javascript",
   "typescriptreact",
   "javascriptreact",
   "vue",
-}
-
-local vscode = require("dap.ext.vscode")
-vscode.type_to_filetypes["node"] = js_filetypes
-vscode.type_to_filetypes["pwa-node"] = js_filetypes
-
-local current_file = vim.fn.expand("%:t")
-for _, language in ipairs(js_filetypes) do
+}) do
   dap.configurations[language] = {
     {
-      name = "Launch file",
+      name = "Launch file using Node.js (nvim-dap) Launch",
       type = "pwa-node",
       request = "launch",
       program = "${file}",
       cwd = "${workspaceFolder}",
     },
     {
-      name = "Attach",
+      name = "Attach to process using Node.js (nvim-dap) Attach",
       type = "pwa-node",
       request = "attach",
       processId = require("dap.utils").pick_process,
       cwd = "${workspaceFolder}",
     },
+    -- requires ts-node to be installed globally or locally
     {
-      name = "tsx (" .. current_file .. ")",
-      type = "node",
+      name = "Launch file Node.js file ts-node/register (nvim-dap)",
+      type = "pwa-node",
       request = "launch",
       program = "${file}",
-      runtimeExecutable = "tsx",
       cwd = "${workspaceFolder}",
-      console = "integratedTerminal",
-      internalConsoleOptions = "neverOpen",
+      runtimeArgs = { "-r", "ts-node/register" },
+    },
+    {
+      name = "Launch Edge (nvim-dap) Launch",
+      type = "pwa-msedge",
+      request = "launch",
+      url = get_user_input(),
+      webRoot = "${workspaceFolder}",
+      sourceMaps = true,
+    },
+    {
+      name = "Launch Chrome (nvim-dap) Launch",
+      type = "pwa-chrome",
+      request = "launch",
+      url = get_user_input(),
+      webRoot = "${workspaceFolder}",
+      sourceMaps = true,
+      -- optional nice-to-haves:
+      resolveSourceMapLocations = {
+        "${workspaceFolder}/**",
+        "!**/node_modules/**",
+      },
       skipFiles = {
         "<node_internals>/**",
         "${workspaceFolder}/node_modules/**",
       },
     },
     {
-      name = "pick script (pnpm)",
-      type = "node",
+      name = "Attach Chrome Attach (nvim-dap)",
+      type = "pwa-chrome",
+      request = "attach",
+      port = get_user_input({
+        prompt = "Enter port: ",
+        default = "9222",
+      }),
+      webRoot = "${workspaceFolder}",
+      -- urlFilter = "http://localhost/some-url-path",
+      sourceMaps = true,
+      -- smartStep = true, -- auto-step through transpiled helper code
+      -- optional nice-to-haves:
+      resolveSourceMapLocations = {
+        "${workspaceFolder}/**",
+        "!**/node_modules/**",
+      },
+
+      -- 🔑 Map paths from sourcemaps -> real files on disk
+      sourceMapPathOverrides = sourceMapPathOverrides,
+
+      skipFiles = {
+        "<node_internals>/**",
+        "**/node_modules/**",
+        "**/old_app*",
+        "**/*.esm-bundler*",
+        "chrome-extension*",
+        "**/559.js",
+        "**/dist/**",
+
+        -- -- The concrete files to be skipped
+        -- "webpack://*/runtime-core.esm-bundler.js",
+        -- "webpack://*/reactivity.esm-bundler.js",
+        -- "webpack-internal://*/shared.esm-bundler.js",
+      },
+    },
+
+    {
+      name = "Chrome: scheduling-mfe (launch)",
+      type = "pwa-chrome",
       request = "launch",
-      runtimeExecutable = "pnpm",
-      runtimeArgs = { "run", pick_script },
-      cwd = "${workspaceFolder}",
+
+      -- Your Vue app URL
+      url = "http://alayadev.localhost/scheduling-mfe",
+
+      -- Project root (make sure :pwd in nvim is this dir)
+      webRoot = "${workspaceFolder}",
+
+      -- Tell js-debug which Chrome to use (Linux)
+      runtimeExecutable = "/usr/bin/google-chrome",
+
+      -- Equivalent of your --user-data-dir arg
+      userDataDir = "/tmp/chrome-nvim-dap-debug",
+
+      -- Sourcemap goodies
+      sourceMaps = true,
+      smartStep = true,
+
+      resolveSourceMapLocations = {
+        "${workspaceFolder}/**",
+        "!**/node_modules/**",
+      },
+
+      -- 🔑 Make .vue SFCs & Docker paths map correctly
+      sourceMapPathOverrides = sourceMapPathOverrides,
+
+      skipFiles = {
+        "<node_internals>/**",
+        "${workspaceFolder}/node_modules/**",
+      },
     },
   }
 end
