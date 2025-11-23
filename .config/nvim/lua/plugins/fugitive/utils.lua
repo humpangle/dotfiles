73982a14e10f5c477c5495c81278a1e3c5f28ab2
Kanmii <maneptha@gmail.com>
2025-11-22 22:29:44 -0500

    nvim git_refresh_cwd: do not echo

--- .config/nvim/lua/plugins/fugitive/utils.lua
+++ .config/nvim/lua/plugins/fugitive/utils.lua
@@ -35,7 +35,6 @@ end
 function M.git_refresh_cwd()
   utils.handle_cant_re_enter_normal_mode_from_terminal_mode(function()
     vim.cmd("Git")
-    print("Git refreshed!")
   end, {
     force = true,
     wipe = true,
