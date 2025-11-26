495b8f540a94c4741d136f9d0121c1f593da8a02
Kanmii <maneptha@gmail.com>
2025-11-26 03:31:50 -0500

    Fix for nvim fugitive show commit under cursor in qf buffer
    When in the qf buffer and fzf-lua is brought up, the cursor immediately
    jumps to the previous non-qf buffer before we can select the commit
    under cursor.
    
    Our workaround is to first copy the commit and then perform required action

--- .config/nvim/lua/plugins/fugitive/utils.lua
+++ .config/nvim/lua/plugins/fugitive/utils.lua
@@ -1,15 +1,15 @@
 local M = {}
 local utils = require("utils")
 
-function M.highlight_text_under_cursor()
-  local text_under_cursor = vim.fn.expand("<cword>")
+function M.highlight_text_under_cursor(text)
+  local text_under_cursor = text or vim.fn.expand("<cword>")
   vim.fn.setreg("/", text_under_cursor)
   vim.cmd("set hlsearch")
   return text_under_cursor
 end
 
 function M.open_commit_under_cursor(split_cmd)
-  local commit_ish = M.highlight_text_under_cursor()
+  local commit_ish = M.highlight_text_under_cursor(vim.fn.getreg(""))
   vim.cmd(split_cmd)
   vim.cmd("Gedit " .. commit_ish)
 end
